048ee7bc03191b14285308c355d012ce228dbd85
vexxy <122513007+vexxy69@users.noreply.github.com>
2023-04-18 16:02:26 +0100

    Delete server.lua

--- thingy/server.lua
+++
@@ -1,9 +0,0 @@
-RegisterServerEvent('999Call')
-AddEventHandler('999Call', function(emergency)
-    local player = source
-    local playerPed = GetPlayerPed(player)
-    local coords = GetEntityCoords(playerPed)
-    local playerName = GetPlayerName(player)
-
-    TriggerClientEvent('showCall', -1, coords, emergency, playerName)
-end)
